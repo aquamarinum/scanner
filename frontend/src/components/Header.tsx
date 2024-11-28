a3b3d40b5a7ffae8c6414654ad54fcd8fe150857
Artemy-sweden <ghosteagle2020@mail.ru>
2024-11-28 17:45:02 +0300

    feat: header login button while not auth

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -7,39 +7,46 @@ import TermsIcon from "./Icons/TermsIcon";
 import OptionsIcon from "./Icons/OptionsIcon";
 import ThemeIcon from "./Icons/ThemeIcon";
 import ProfileIcon from "./Icons/ProfileIcon";
+import { useAuth } from "../hooks/useAuth";
+import Button from "./Button";
+import Title from "./Title";
+import Paragraph from "./Paragraph";
 
 const Header = () => {
+  const { authToken } = useAuth();
   return (
-    <nav>
-      <Link to="/">
-        <LogoIcon />
-      </Link>
-      <ul>
-        <li>
-          <NavLink to="/">
-            <HomeIcon />
-          </NavLink>
-        </li>
-        <li>
-          <NavLink to="/categories">
-            <CategoriesIcon />
-          </NavLink>
-        </li>
-        <li>
-          <NavLink to="/terms">
-            <TermsIcon />
-          </NavLink>
-        </li>
-        <li>
-          <NavLink to="/options">
-            <OptionsIcon />
-          </NavLink>
-        </li>
-      </ul>
-      <Link to="/profile">
-        <ProfileIcon />
-      </Link>
-    </nav>
+    <header>
+      <nav>
+        <Link to="/">
+          <LogoIcon />
+        </Link>
+        <ul>
+          <li>
+            <NavLink to="/">
+              <HomeIcon />
+            </NavLink>
+          </li>
+          <li>
+            <NavLink to="/categories">
+              <CategoriesIcon />
+            </NavLink>
+          </li>
+          <li>
+            <NavLink to="/terms">
+              <TermsIcon />
+            </NavLink>
+          </li>
+          <li>
+            <NavLink to="/options">
+              <OptionsIcon />
+            </NavLink>
+          </li>
+        </ul>
+        <Link to="/profile">
+          {authToken ? <ProfileIcon /> : <Paragraph>Login</Paragraph>}
+        </Link>
+      </nav>
+    </header>
   );
 };
 
