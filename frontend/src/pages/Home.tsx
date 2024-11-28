d2d228c9fc6a3692b9b632cb5985b3393bc66da7
Artemy-sweden <ghosteagle2020@mail.ru>
2024-11-28 11:55:30 +0300

    fix: backend gitingore

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,7 +1,20 @@
 import React from "react";
+import { useSignInOut } from "../hooks/useSignInOut";
+import { useAuth } from "../hooks/useAuth";
 
 const Home = () => {
-  return <div>Home</div>;
+  const { logout } = useSignInOut();
+  const { signout } = useAuth();
+  return (
+    <button
+      onClick={() => {
+        logout();
+        signout();
+      }}
+    >
+      logout
+    </button>
+  );
 };
 
 export default Home;
