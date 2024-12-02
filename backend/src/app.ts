5542159e2d488b9040fccbc4d3de8f58e5ebf17e
Artemy-sweden <ghosteagle2020@mail.ru>
2024-12-02 20:18:33 +0300

    feat: GET routes for users, user & vulns

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -8,13 +8,41 @@ app.get("/", (req, res) => {
   res.send("Hello World!");
 });
 
-app.get("/test", (req, res) => {
-  pool.query("SELECT * FROM Test", (err, results) => {
+app.get("/vulnerabilities", (req, res) => {
+  pool.query("SELECT * FROM Vulnerabilities", (err, results) => {
     if (err) {
       return res.status(500).send(err);
     }
     res.json(results);
   });
+});
+
+app.get("/users", (req, res) => {
+  pool.query("SELECT * FROM Users", (err, results) => {
+    if (err) {
+      return res.status(500).send(err);
+    }
+    res.json(results);
+  });
+});
+
+app.get("/users/:id", (req, res) => {
+  const userId = req.params.id;
+  pool.query(
+    "SELECT * FROM users WHERE userId = ?",
+    [userId],
+    (err, results) => {
+      if (err) {
+        return res.status(500).send(err);
+      }
+      //@ts-ignore
+      if (results.length === 0) {
+        return res.status(404).send("User not found");
+      }
+      //@ts-ignore
+      res.json(results[0]);
+    }
+  );
 });
 
 app.listen(port, () => {
